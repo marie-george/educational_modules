af72edfa96504bc4deddb22829db91e5cf81e995
marie-george <maria85night@gmail.com>
2023-09-10 22:20:39 +0300

    added docker

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-yzi%4y-mqt-w-9s*7qau68j7$)b4g$t%fh28-!=+3q)fv$mo2w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
